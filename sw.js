0449d7b2b63b765a79b2639ffdec173aea570e4f
Jonatan Ribot <proferibotmusic@hotmail.com>
2026-09-20 13:41:52 -0300

    Change cache version to v3 in service worker
    Updated service worker cache version from v2 to v3.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open('sler-cache-v2') // Cambia v1 por v2
+        caches.open('ler-cache-v3') // Cambia v1 por v2
             return cache.addAll([
                 'index.html',
                 'app.js',
